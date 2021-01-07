9460112b82ac502869ad81c19e57fc44c89c69cd
张伟伟 <zhangweiwei@irietsvpc.com>
2021-01-07 16:42:20 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div  class="hello-world">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite + Antd 2" />
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style scoped>
+  .hello-world{
+    text-align: center;
+  }
+</style>
